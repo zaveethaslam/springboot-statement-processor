3757b96f901e2b753eae49976c11a11ba9749670
Zaveeth Aslam <zaveethaslam@gmail.com>
2020-08-16 23:22:07 +0530

    Added Cors

--- src/main/java/com/rabobank/customer/statementprocessor/StatementProcessorApplication.java
+++ src/main/java/com/rabobank/customer/statementprocessor/StatementProcessorApplication.java
@@ -2,6 +2,10 @@ package com.rabobank.customer.statementprocessor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class StatementProcessorApplication {
@@ -10,4 +14,12 @@ public class StatementProcessorApplication {
 		SpringApplication.run(StatementProcessorApplication.class, args);
 	}
 
+	@SuppressWarnings("deprecation")
+	@Bean public WebMvcConfigurer corsConfigurer() { return new
+	  WebMvcConfigurerAdapter() {
+	  
+	  @Override public void addCorsMappings(CorsRegistry registry) {
+	  registry.addMapping("/process").allowedOrigins(
+	  "http://localhost:4200"); } }; }
+	
 }
--- src/main/java/com/rabobank/customer/statementprocessor/exception/GlobalExceptionHandler.java
+++ src/main/java/com/rabobank/customer/statementprocessor/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,6 +18,16 @@ import com.rabobank.customer.statementprocessor.api.dto.ResponseMessage;
 public class GlobalExceptionHandler {
 	
 	private Logger logger = LoggerFactory.getLogger(StatementProcessorController.class);
+	
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+	public @ResponseBody ResponseMessage handleInvalidFormatException(final Exception exception,
+			final HttpServletRequest request) {
+		logger.error(HttpStatus.BAD_REQUEST + " Bad Request");
+		return ResponseMessage.builder()
+				.result("BAD_REQUEST")
+				.build();
+	}
 
 	@ExceptionHandler(Exception.class)
 	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
@@ -29,3 +40,5 @@ public class GlobalExceptionHandler {
 	}
 	
 }
+
+
